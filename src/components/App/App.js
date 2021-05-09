9b48d0397dc825a19a8b4c29992f0c2d2b234e31
Alexandr Raspertov <raspertovalexandr@yandex.ru>
2021-05-10 01:42:54 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Popup from '../Popup/Popup';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Popup />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,11 @@
+import './Popup.css';
+
+function Popup() {
+  return (
+    <header>
+      
+    </header>
+  );
+}
+
+export default Popup;
